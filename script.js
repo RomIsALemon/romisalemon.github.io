4b49ff984cafae2b6d94807099d200a3844e1d11
RomIsALemon <152632066+RomIsALemon@users.noreply.github.com>
2025-11-27 21:45:59 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let ssTimer;
+const screensaver = document.getElementById("screensaver");
+const initTitle = document.title
+
+document.addEventListener("mousemove", function(e) {
+    setTimer();
+});
+
+document.addEventListener("keypress", function(e) {
+    setTimer()
+});
+
+function setTimer() {
+    document.title = initTitle;
+    clearTimeout(ssTimer);
+    screensaver.style.display = 'none';
+    ssTimer = setTimeout(() => {
+        screensaver.style.display = 'flex';
+        document.title=initTitle + " - Currently Asleep";
+    }, 120000);
+}
+
+setTimer();
+
+function sendtopage(name) {
+    switch(name) {
+        case "ComingSoon":
+            window.open("ComingSoon.html", "_self")
+            break;
+        case "SaveState":
+            window.open("SaveState.html", "_self");
+            break;
+        case "FishEatGame":
+            window.open("AlwaysASmallerFish.html", "_blank")
+            break;
+        case "Icarus":
+            window.open("IcarusCrispyFeathers.html", "_self")
+    }
+}
